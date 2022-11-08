a611f6aa56e1027d5fd08f39784a13cab263b87c
AmosOvadia <amosovadya12@gmail.com>
2022-11-08 17:55:52 +0200

    Initial code + Welcome functions

---
+++ dotNet5783_5646/Stage0/Program1_5646.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Stage0
+{
+   partial class program
+   {   
+        static void Main(string[] args)
+        {
+            Welcome1_5646();
+            Welcome2_5646();
+            Console.ReadKey();
+        }
+        static partial void Welcome2_5646();
+        private static void Welcome1_5646()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console applications", userName);
+        }
+    }
+}
